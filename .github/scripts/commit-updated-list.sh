502a1ce705814ed3999337a2d1358777ecc3d131
Douglas Domingos <doug.domingossilva@gmail.com>
2024-11-24 08:51:45 -0300

    fix: change commit author only for action commit

--- .github/scripts/commit-updated-list.sh
+++ .github/scripts/commit-updated-list.sh
@@ -3,18 +3,14 @@
 # Automatically push any existent changes to the blog repository.
 # Authors: @dougdomingos
 
-USERNAME='github-actions[bot]'
-EMAIL='github-actions[bot]@users.noreply.github.com'
-
-git config user.name "${USERNAME}"
-git config user.email "${EMAIL}"
+readonly COMMIT_AUTHOR='github-actions[bot] <github-actions[bot]@users.noreply.github.com>'
 
 git diff --quiet
 change_status=$?
 
 if [ $change_status -ne 0 ]; then
     git add ./data/projects.yaml
-    git commit -m "chore(actions): update project list"
+    git commit -m "chore(actions): update project list" --author="$COMMIT_AUTHOR"
     git push
 else
     echo "No changes detected. Nothing to commit."
