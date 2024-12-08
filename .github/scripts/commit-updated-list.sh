ffda4545a23f14b212e0dda7434f6caa29f29652
Douglas Domingos <doug.domingossilva@gmail.com>
2024-12-08 08:45:17 -0300

    fix(actions): broken commit author configuration on project list update

--- .github/scripts/commit-updated-list.sh
+++ .github/scripts/commit-updated-list.sh
@@ -3,14 +3,12 @@
 # Automatically push any existent changes to the blog repository.
 # Authors: @dougdomingos
 
-readonly COMMIT_AUTHOR='github-actions[bot] <github-actions[bot]@users.noreply.github.com>'
-
 git diff --quiet
 change_status=$?
 
 if [ $change_status -ne 0 ]; then
     git add ./data/projects.yaml
-    git commit -m "chore(actions): update project list" --author="$COMMIT_AUTHOR"
+    git commit -m "chore(actions): update project list"
     git push
 else
     echo "No changes detected. Nothing to commit."
